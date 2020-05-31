8ecbb291a61e76ffedf5c39a8f929cd55df2589f
Norman Koch <norman.koch@tu-dresden.de>
2020-05-31 21:10:06 +0200

    added wmctrl

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install -y python3 python3-pip libasound2-dev libspeexdsp-dev qalc xsel sed
+sudo apt install -y python3 python3-pip libasound2-dev libspeexdsp-dev qalc xsel sed wmctrl
 sudo apt install -y libxml2-dev libxslt1-dev zlib1g-dev libffi-dev
 sudo apt install -y libsasl2-dev libldap2-dev libssl-dev portaudio19-dev
 
@@ -10,4 +10,4 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
 sudo apt install -y libttspico-utils
 sudo pip3 install pyopenssl
-sudo pip3 install secrets colored wmctrl pyperclip pyautogui scipy halo webrtcvad wave pyaudio numpy deepspeech==0.7.1 zahlwort2num commands
+sudo pip3 install secrets colored wmctrl pyperclip pyautogui scipy halo webrtcvad wave pyaudio numpy deepspeech==0.7.1 zahlwort2num
