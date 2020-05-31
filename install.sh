50103433d9d025e740c404ecab2ab5a505bc2f6b
Norman Koch <norman.koch@tu-dresden.de>
2020-05-31 20:49:43 +0200

    added install

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt install python3 python3-pip libasound2-dev libspeexdsp-dev qalc xsel sed
+sudo apt-get install libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
 sudo apt install libttspico-utils
+sudo pip3 install pyopenssl
 sudo pip3 install secrets colored wmctrl pyperclip pyautogui scipy halo webrtcvad wave pyaudio numpy deepspeech==0.7.1 zahlwort2num
