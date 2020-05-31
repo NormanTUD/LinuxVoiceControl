7828218e13c001bed3062993610856436f1905d3
Norman Koch <norman.koch@tu-dresden.de>
2020-05-31 20:54:21 +0200

    added more dependencies

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo apt install python3 python3-pip libasound2-dev libspeexdsp-dev qalc xsel sed
-sudo apt-get install libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
-sudo apt install libttspico-utils
+sudo apt install -y python3 python3-pip libasound2-dev libspeexdsp-dev qalc xsel sed
+sudo apt install -y libxml2-dev libxslt1-dev zlib1g-dev libffi-dev
+sudo apt install -y libsasl2-dev libldap2-dev libssl-dev portaudio19-dev
+sudo apt install -y libttspico-utils
 sudo pip3 install pyopenssl
 sudo pip3 install secrets colored wmctrl pyperclip pyautogui scipy halo webrtcvad wave pyaudio numpy deepspeech==0.7.1 zahlwort2num
