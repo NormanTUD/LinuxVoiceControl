32b14e448a1ed6a43ddf8f5281d959b0888e0388
Norman Koch <norman.koch@tu-dresden.de>
2020-06-19 12:23:36 +0200

    added install stuff

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install -y python3 python3-pip libasound2-dev libspeexdsp-dev qalc xsel sed wmctrl
-sudo apt install -y libxml2-dev libxslt1-dev zlib1g-dev libffi-dev
+sudo apt install -y libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libatlas-base-dev
 sudo apt install -y libsasl2-dev libldap2-dev libssl-dev portaudio19-dev python3-tk sox libsox-fmt-all
 
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
@@ -9,5 +9,5 @@ echo "enable non-free repositories!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
 sudo apt install -y libttspico-utils
-sudo pip3 install pyopenssl
+sudo pip3 install pyopenssl psutil babel
 sudo pip3 install secrets colored wmctrl pyperclip pyautogui scipy halo webrtcvad wave pyaudio numpy deepspeech==0.7.1 zahlwort2num wikipedia-api
